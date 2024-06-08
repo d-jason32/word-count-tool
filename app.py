05dd43a0f0d27e5e0a2a6104fa8b658a8c72de51
Jason Devaraj <jdevaraj01@gmail.com>
2024-06-08 16:46:42 -0400

    Finished main function

--- app.py
+++ app.py
@@ -14,6 +14,14 @@ parser.add_argument('-c',
 parser.add_argument('-l',
                     '--line',
                     action='store_true')
+
+parser.add_argument('-w',
+                    '--word',
+                    action='store_true')
+
+parser.add_argument('-m',
+                    '--character',
+                    action='store_true')
                     
 args = parser.parse_args()
 
@@ -26,3 +34,14 @@ if args.line:
     with open(args.filename, 'rb') as file:
         content = file.readlines()
         print(f"{len(content)} {args.filename}")
+
+if args.word:
+    with open(args.filename, 'rb') as file:
+        content = file.read()
+        print(f"{len(content.split())} {args.filename}")
+
+if args.character:
+    with open(args.filename, 'rb') as file:
+        content = file.read(-1)
+        decoded = content.decode('utf-8')
+        print(f"{len(decoded)} {args.filename}")
